c23a27a7e094b314cc3643fbf10c663c72570549
Philippe Luchansky <philuchansky@gmail.com>
2016-11-28 15:43:35 -0800

    create and stop oscillator with button down / up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var keyboard = document.querySelector('#keyboard')
 var keys = document.querySelectorAll('#keyboard .key button')
+var oscillator = null
+
 
 // event listeners for all keys:
 keys.forEach(function(key) {
@@ -7,10 +9,24 @@ keys.forEach(function(key) {
   key.addEventListener('mouseup', stopSound)
 })
 
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var vca = audioCtx.createGain()
+vca.gain.value = 1
+vca.connect(audioCtx.destination)
+
 function playSound() {
   console.log("Start playing!")
+  // create Oscillator node
+  oscillator = audioCtx.createOscillator();
+  oscillator.type = oscillator.SINE;
+  oscillator.frequency.value = 220; // value in hertz
+  oscillator.connect(vca)
+  oscillator.start();
+
 }
 
 function stopSound() {
   console.log("Stop playing!")
+  oscillator.stop();
 }
