51cfd0e2bacdce5b6b18be2a1ea5c170ed52dfc6
Philippe Luchansky <philuchansky@gmail.com>
2016-11-28 15:09:27 -0800

    event listeners for mousedown + mouseup on all keys

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("script loaded")
+var keyboard = document.querySelector('#keyboard')
+var keys = document.querySelectorAll('#keyboard .key button')
+
+// event listeners for all keys:
+keys.forEach(function(key) {
+  key.addEventListener('mousedown', playSound)
+  key.addEventListener('mouseup', stopSound)
+})
+
+function playSound() {
+  console.log("Start playing!")
+}
+
+function stopSound() {
+  console.log("Stop playing!")
+}
