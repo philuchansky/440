1c4409cd486864a06da8eeca2d5eb7a542aff325
Philippe Luchansky <philuchansky@gmail.com>
2016-11-29 13:55:56 -0800

    release time optimized, keys can play with different release times in seconds.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
 var keyboard = document.querySelector('#keyboard')
 var keys = document.querySelectorAll('#keyboard button.key')
 var waveformSelect = document.querySelector('#waveform-select')
-var oscillator = null
+var release = document.querySelector('#release')
 
-// create web audio api context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-var vca = audioCtx.createGain()
-vca.gain.value = 0.3
-vca.connect(audioCtx.destination)
 
-function attack(freq) {
-  oscillator = audioCtx.createOscillator()
-  oscillator.type = oscillator[waveformSelect.value]
-  oscillator.frequency.value = freq
-  oscillator.connect(vca)
-  oscillator.start()
 
+function release(time) {
+  oscillator.stop(time);
+
+  // var fadeOut = setInterval(function(){
+  //   // vca.gain.value -= 0.5
+  // }, 10)
   //
-  console.log("Playing at", freq + "hz")
-}
-
-function release() {
-  oscillator.stop();
+  // setTimeout(function() {
+  //   clearInterval(fadeOut)
+  // }, releaseTime)
 
   //
   console.log("Stop playing!")
@@ -31,7 +24,52 @@ function release() {
 keys.forEach(function(key) {
   key.addEventListener('mousedown', function() {
     this.freq = Number(this.dataset.freq)
-    attack(this.freq)
+    new Note(this.freq)
   })
-  key.addEventListener('mouseup', release)
+
+  // key.addEventListener('mouseup', function() {
+  //   release(adsr.release())
+  // })
 })
+
+function Note(freq) {
+  // adsr:
+  this.release = release.value / 20
+  // this.release = 1
+
+
+
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+
+  var vca = audioCtx.createGain()
+  vca.gain.value = 1
+  vca.connect(audioCtx.destination)
+
+  var oscillator = audioCtx.createOscillator()
+  oscillator.type = oscillator[waveformSelect.value]
+  oscillator.frequency.value = freq
+  oscillator.connect(vca)
+  oscillator.start()
+
+  var releaseTime = (this.release)
+  console.log("Release time:", releaseTime)
+  var initialGain = vca.gain.value
+  console.log("Initial Gain:", initialGain)
+
+  var attenuationRate = (0.01 / (releaseTime))
+
+
+  var fadeOut = setInterval(function() {
+    vca.gain.value -= attenuationRate
+  }, 10)
+
+  oscillator.stop(releaseTime)
+
+  setTimeout(function() {
+    console.log("Ending Gain:", vca.gain.value)
+    clearInterval(fadeOut)
+    audioCtx.close()
+  }, releaseTime * 1000)
+  //
+  console.log("Playing at", freq + "hz")
+}
