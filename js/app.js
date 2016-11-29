347c7ca9e0aba10f083ca7f4769f5ef0c3de9d3d
Philippe Luchansky <philuchansky@gmail.com>
2016-11-28 16:00:53 -0800

    waveform select tag

--- js/app.js
+++ js/app.js
@@ -3,24 +3,27 @@ var keys = document.querySelectorAll('#keyboard button.key')
 var oscillator = null
 
 // create web audio api context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 var vca = audioCtx.createGain()
-vca.gain.value = 1
+vca.gain.value = 0.3
 vca.connect(audioCtx.destination)
 
 function attack(freq) {
-  console.log("Playing at", freq + "hz")
-  // create Oscillator node
-  oscillator = audioCtx.createOscillator();
-  oscillator.type = oscillator.SAWTOOTH;
-  oscillator.frequency.value = freq; // value in hertz
+  oscillator = audioCtx.createOscillator()
+  oscillator.type = oscillator.SAWTOOTH
+  oscillator.frequency.value = freq
   oscillator.connect(vca)
-  oscillator.start();
+  oscillator.start()
+
+  //
+  console.log("Playing at", freq + "hz")
 }
 
 function release() {
-  console.log("Stop playing!")
   oscillator.stop();
+
+  //
+  console.log("Stop playing!")
 }
 
 // event listeners for all keys:
